f16e12bd6867b807e16825306c07fa33e52a5ba7
dgTr3 <Edugta31@gmail.com>
2026-03-05 21:08:29 -0600

    completo

--- Do-While18/Program.cs
+++ Do-While18/Program.cs
@@ -5,7 +5,24 @@ do
     Console.WriteLine("1. Saluddar");
     Console.WriteLine("2. Mostrar número aleatorio");
     Console.WriteLine("0. Salir");
-    opciones=int.Parse(Console.ReadLine());
+    opciones = int.Parse(Console.ReadLine());
+
+    switch (opciones)
+    {
+        case 1:
+            Console.WriteLine("Hola!!");
+            break;
+        case 2:
+            Random random = new Random();
+            int numeroAleatorio = random.Next(1, 101);
+            Console.WriteLine($"Número aleatorio: {numeroAleatorio}");
+            break;
+
+        case 3:
+            Console.WriteLine("Saliendo...");
+            break;
 
 
+    }
 }
+while (opciones != 0);
