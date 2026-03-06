13eba68623088933250bd287b01c70412c8a9b9d
dgTr3 <Edugta31@gmail.com>
2026-03-05 21:05:28 -0600

    opciones

--- Do-While18/Program.cs
+++ Do-While18/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Menú repetitivo");
+int opciones;
+do
+{
+    Console.WriteLine("1. Saluddar");
+    Console.WriteLine("2. Mostrar número aleatorio");
+    Console.WriteLine("0. Salir");
+    opciones=int.Parse(Console.ReadLine());
+
+
+}
